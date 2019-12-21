1cbbf26db6220f8d822cf36673adc8dd29af36b3
Ромасс Ольга Дмитриевна <romass@tochka.com>
2019-12-21 15:43:05 +0500

    fixed

--- lab3.ino
+++ lab3.ino
@@ -42,10 +42,7 @@ void setup() {
 void loop() {
   if (button.wasUp())
   {
-    for(int i=0; i<=angle; i++){
-        servo.write(i);
-        delay(servoDelay);
-    }
+    turnServo();
     delay(300);
     angle = 0;
     servoDelay = maxServoDelay;
@@ -56,14 +53,25 @@ void loop() {
   {
     angle = maxAngle;
     servoDelay = max(servoDelay - delayServoStep, 1);
-    Serial.println(servoDelay);
     cellToDrawNamber = ((maxServoDelay - servoDelay)/delayServoStep) - 1;
     point.y = servoDelay / boardSizeY;
-    for(int i=0; i<=8; i++){
-        point.x = i;
-        drawPoint(point);
-    }
+    drawRow();
     delay(drawDelay);
+  }
+}
+
+void turnServo()
+{
+  for(int i=0; i<=angle; i++){
+      servo.write(i);
+      delay(servoDelay);
+  }
+}
+
+void drawRow(){
+  for(int i=0; i<=8; i++){
+      point.x = i;
+      drawPoint(point);
   }
 }
 
